22e8ec0b877d95527ca412abcde9f35e21b3c0f0
Sathiya Sri <sathiyasri@example.com>
2026-06-15 16:23:07 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("🚀 Task Manager API is running");
+});
 
 const FILE = "tasks.json";
 
@@ -98,6 +101,8 @@ app.delete("/tasks/:id", (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log("🚀 Server Running on Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server running on", PORT);
 });
